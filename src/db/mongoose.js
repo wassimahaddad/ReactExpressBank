cade438e91c84f11408d652d930ac50c42dd4b79
wassimahaddad <wass75@gmail.com>
2021-04-24 01:39:15 +0300

    commit

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,11 +1,8 @@
 const mongoose = require("mongoose");
-const { user, password } = require("../../config/keys");
+const { uri } = require("../../config/keys");
 
-mongoose.connect(
-  `mongodb+srv://${user}:${password}@cluster0.1ki7z.mongodb.net/myBankDatabase?retryWrites=true&w=majority`,
-  {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
