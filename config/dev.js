c6beae1be46ed48dadd8e42ea1a825665a8a5c2b
wassimahaddad <wass75@gmail.com>
2021-04-24 10:40:24 +0300

    commit

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,5 @@
 module.exports = {
   user: "dbUser",
   password: "My1stClut3r",
+  srv: "1ki7z",
 };
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
-const { user, password } = require("../../config/keys");
+const { user, password, srv } = require("../../config/keys");
 
 mongoose.connect(
-  `mongodb+srv://${user}:${password}@cluster0.1ki7z.mongodb.net/myBankDatabase?retryWrites=true&w=majority`,
+  `mongodb+srv://${user}:${password}@cluster0.${srv}.mongodb.net/myBankDatabase?retryWrites=true&w=majority`,
   {
     useNewUrlParser: true,
     useCreateIndex: true,
